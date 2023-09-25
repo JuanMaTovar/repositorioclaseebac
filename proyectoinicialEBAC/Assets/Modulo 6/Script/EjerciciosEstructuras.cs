4af325c3dd8264a1deb8a523ba0e2e9c5088bffa
JuanMaTovar <91153519+JuanMaTovar@users.noreply.github.com>
2023-09-24 20:37:15 -0700

    modulo11
    Actividades del módulo 11

---
+++ proyectoinicialEBAC/Assets/Modulo 6/Script/EjerciciosEstructuras.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjerciciosEstructuras : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        FuncionLista(20, 1, 20);
+        FuncionDescentente(5,1,10);
+        FuncionHashSet(10, 1, 10);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //Lista con valores inferior superios
+    public List<int> FuncionLista(int tamaño, int rangoInf, int rangoSup)
+    {
+        List<int> listaNumeros1 = new List<int>();
+        for (int i = 0; i <tamaño; i++)
+        {
+            listaNumeros1.Add(Random.Range(rangoInf, rangoSup));
+        }
+        foreach (var numero in listaNumeros1)
+        {
+            Debug.Log(numero);
+        }
+        return listaNumeros1;
+    }
+    //Ordenar los numeros de manera descente
+    public List<int> FuncionDescentente(int tamaño, int rangoInf, int rangoSup)
+    {
+        List<int> listaNumeros2 = new List<int>();
+        for (int i = 0; i < tamaño; i++)
+        {
+            listaNumeros2.Add(Random.Range(rangoInf, rangoSup));
+        }
+        listaNumeros2.Sort();
+        listaNumeros2.Reverse();
+        foreach (var numero in listaNumeros2)
+        {
+            Debug.Log(numero);
+        }
+        return listaNumeros2;
+    }
+   //HashSet sin numeros repetidos
+    public HashSet<int> FuncionHashSet(int tamaño, int rangoInf, int rangoSup)
+    {
+        HashSet<int> hashSetNum = new HashSet<int>();
+        for (int i = 0; i < tamaño; i++)
+        {
+            hashSetNum.Add(Random.Range(rangoInf, rangoSup));
+        }
+        foreach (var numero in hashSetNum)
+        {
+       
+            Debug.Log(numero);
+        }
+
+        return hashSetNum;
+    }
+   
+}
